b081ca368d3cc254d259a82a16fb416895ef7604
monoflop <monoflop@protonmail.com>
2022-12-04 18:34:38 +0100

    Removed tu-blockchain project

--- tu-blockchain/src/main/java/com/philippkutsch/tuchain/blockchain/HashMiningStepInterface.java
+++
@@ -1,5 +0,0 @@
-package com.philippkutsch.tuchain.blockchain;
-
-public interface HashMiningStepInterface {
-    void onHashCreated(long number, byte[] hash);
-}
--- tu-blockchain/startWithDefaultConfig.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar target/tu-blockchain-1.0-SNAPSHOT.jar node --config config.json --verbose
