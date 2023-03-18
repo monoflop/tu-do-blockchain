64a2c8c0c5c209e488864b8cc9d86e3572458288
monoflop <monoflop@protonmail.com>
2023-03-18 23:28:08 +0100

    Disabled DSSimulatorTest

--- tu-blockchain-node/src/test/java/com/philippkutsch/tuchain/dss/DSSimulatorTest.java
+++ tu-blockchain-node/src/test/java/com/philippkutsch/tuchain/dss/DSSimulatorTest.java
@@ -1,10 +1,12 @@
 package com.philippkutsch.tuchain.dss;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 
+@Ignore
 public class DSSimulatorTest {
     @Test
     public void testSimulate() throws IOException, InterruptedException {
